04e568ff700b1b44365e8487b9d8c6d04e581b92
Youritch <86657843+Youritch@users.noreply.github.com>
2024-01-02 09:45:17 +0100

    change translation key

--- src/main/java/com/harby/halocraft/core/HaloKeybinds.java
+++ src/main/java/com/harby/halocraft/core/HaloKeybinds.java
@@ -4,7 +4,7 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 
 public class HaloKeybinds {
-    public static final KeyMapping RELOAD_KEY = new KeyMapping("key.reload", InputConstants.KEY_R, "key.categories.misc");
-    public static final KeyMapping SHOOTING_KEY = new KeyMapping("key.shoot", InputConstants.KEY_F, "key.categories.misc");
+    public static final KeyMapping RELOAD_KEY = new KeyMapping("key.halocraft.reload", InputConstants.KEY_R, "key.categories.misc");
+    public static final KeyMapping SHOOTING_KEY = new KeyMapping("key.halocraft.shoot", InputConstants.KEY_F, "key.categories.misc");
 
 }
