caed8a2226607f197ef535895932f3f31fc50a48
Youritch <86657843+Youritch@users.noreply.github.com>
2024-01-02 16:14:23 +0100

    add bullet type

---
+++ src/main/java/com/harby/halocraft/core/BulletType.java
@@ -0,0 +1,10 @@
+package com.harby.halocraft.core;
+
+public enum BulletType {
+    NONE,
+    BULLET,
+    EXPLOSIVE_BULLET,
+    FIRE_BULLET,
+    FROZEN_BULLET,
+    PENETRATING_BULLET
+}
--- src/main/java/com/harby/halocraft/core/HaloItems.java
+++ src/main/java/com/harby/halocraft/core/HaloItems.java
@@ -13,33 +13,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HaloItems {
-    public  static  final List<Item> HALO_ITEMS = new ArrayList<>();
-    public  static  final List<Gun> GUNS_ITEMS = new ArrayList<>();
+    public static final List<Item> HALO_ITEMS = new ArrayList<>();
+    public static final List<Gun> GUNS_ITEMS = new ArrayList<>();
 
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, HaloCraft.MODID);
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
 
-    public  static final RegistryObject<Item> BULLET = ITEMS.register("bullet",
-            () -> new ItemBase( new Item.Properties()));
-    public  static final RegistryObject<Item> EXPLOSIVE_BULLET = ITEMS.register("explosive_bullet",
-            () -> new ItemBase( new Item.Properties()));
-    public  static final RegistryObject<Item> FIRE_BULLET = ITEMS.register("fire_bullet",
-            () -> new ItemBase( new Item.Properties()));
-    public  static final RegistryObject<Item> FROZEN_BULLET = ITEMS.register("frozen_bullet",
-            () -> new ItemBase( new Item.Properties()));
-    public  static final RegistryObject<Item> PENETRATING_BULLET = ITEMS.register("penetrating_bullet",
-            () -> new ItemBase( new Item.Properties()));
+    public static final RegistryObject<Item> BULLET = ITEMS.register("bullet",
+            () -> new BulletItem(new Item.Properties(), BulletType.BULLET));
+    public static final RegistryObject<Item> EXPLOSIVE_BULLET = ITEMS.register("explosive_bullet",
+            () -> new BulletItem(new Item.Properties(), BulletType.EXPLOSIVE_BULLET));
+    public static final RegistryObject<Item> FIRE_BULLET = ITEMS.register("fire_bullet",
+            () -> new BulletItem(new Item.Properties(), BulletType.FIRE_BULLET));
+    public static final RegistryObject<Item> FROZEN_BULLET = ITEMS.register("frozen_bullet",
+            () -> new BulletItem(new Item.Properties(), BulletType.FROZEN_BULLET));
+    public static final RegistryObject<Item> PENETRATING_BULLET = ITEMS.register("penetrating_bullet",
+            () -> new BulletItem(new Item.Properties(), BulletType.PENETRATING_BULLET));
 
 
-    public  static final RegistryObject<Item> ASSAULT_RIFFLE = ITEMS.register("assault_riffle",
-            () -> new AssaultRifle( new Item.Properties()));
-    public  static final RegistryObject<Item> SNIPER_RIFFLE = ITEMS.register("sniper_riffle",
-            () -> new SniperRiffle( new Item.Properties(),false));
-    public  static final RegistryObject<Item> GOLDEN_SNIPER_RIFFLE = ITEMS.register("golden_sniper_riffle",
-            () -> new SniperRiffle( new Item.Properties(),true));
+    public static final RegistryObject<Item> ASSAULT_RIFFLE = ITEMS.register("assault_riffle",
+            () -> new AssaultRifle(new Item.Properties()));
+    public static final RegistryObject<Item> SNIPER_RIFFLE = ITEMS.register("sniper_riffle",
+            () -> new SniperRiffle(new Item.Properties(), false));
+    public static final RegistryObject<Item> GOLDEN_SNIPER_RIFFLE = ITEMS.register("golden_sniper_riffle",
+            () -> new SniperRiffle(new Item.Properties(), true));
 
 
     private static RegistryObject<Item> block(RegistryObject<Block> block) {
