1a2f3c1a1e0197ada70bde9a0c89749688c368da
Youritch <86657843+Youritch@users.noreply.github.com>
2024-01-02 09:44:22 +0100

    add auto register for itemBlock

--- src/main/java/com/harby/halocraft/core/HaloBlocks.java
+++ src/main/java/com/harby/halocraft/core/HaloBlocks.java
@@ -2,6 +2,9 @@ package com.harby.halocraft.core;
 
 import com.harby.halocraft.HaloBlocks.BeamEmitter;
 import com.harby.halocraft.HaloCraft;
+import com.harby.halocraft.HaloItems.BlockItemBase;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SoundType;
@@ -11,32 +14,46 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class HaloBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, HaloCraft.MODID);
 
+    /**
+     * Registers a block and his item
+     *
+     * @param name  the name of the block to be registered
+     * @param block the supplier function that creates the block object
+     * @return the registered block object
+     */
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        HaloItems.ITEMS.register(name, () -> new BlockItemBase(toReturn.get(), new Item.Properties()));
+        return toReturn;
+    }
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
 
 
-    public static final RegistryObject<Block> CONCRETE_BRICK = BLOCKS.register("concrete_brick", () -> new Block(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> CONCRETE_BRICK_POLISHED = BLOCKS.register("concrete_brick_polished", () -> new Block(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> CONCRETE_LAYERED = BLOCKS.register("concrete_layered", () -> new Block(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> CONCRETE_LAYERED_SECOND = BLOCKS.register("concrete_layered_second", () -> new Block(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> CONCRETE_BRICK = registerBlock("concrete_brick", () -> new Block(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> CONCRETE_BRICK_POLISHED = registerBlock("concrete_brick_polished", () -> new Block(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> CONCRETE_LAYERED = registerBlock("concrete_layered", () -> new Block(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> CONCRETE_LAYERED_SECOND = registerBlock("concrete_layered_second", () -> new Block(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
 
-    public static final RegistryObject<Block> ASPHALT = BLOCKS.register("asphalt", () -> new Block(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> CONCRETE_YELLOW_DOUBLE = BLOCKS.register("concrete_yellow_double", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> WHITE_LINE = BLOCKS.register("white_line", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> WHITE_LINE_DOTS = BLOCKS.register("white_line_dot", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> YELLOW_DOUBLE = BLOCKS.register("yellow_double", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> YELLOW_DOUBLE_DOT = BLOCKS.register("yellow_double_dot", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> YELLOW_LINE = BLOCKS.register("yellow_line", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> ASPHALT = registerBlock("asphalt", () -> new Block(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> CONCRETE_YELLOW_DOUBLE = registerBlock("concrete_yellow_double", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> WHITE_LINE = registerBlock("white_line", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> WHITE_LINE_DOTS = registerBlock("white_line_dot", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> YELLOW_DOUBLE = registerBlock("yellow_double", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> YELLOW_DOUBLE_DOT = registerBlock("yellow_double_dot", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> YELLOW_LINE = registerBlock("yellow_line", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
 
 
-    public static final RegistryObject<Block> DENSE_ASTEROID = BLOCKS.register("dense_asteroid", () -> new Block(BlockBehaviour.Properties.of().strength(2f,6f).sound(SoundType.STONE)));
-    public static final RegistryObject<Block> ASTEROID = BLOCKS.register("asteroid", () -> new Block(BlockBehaviour.Properties.of().strength(2f,4f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> DENSE_ASTEROID = registerBlock("dense_asteroid", () -> new Block(BlockBehaviour.Properties.of().strength(2f, 6f).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> ASTEROID = registerBlock("asteroid", () -> new Block(BlockBehaviour.Properties.of().strength(2f, 4f).sound(SoundType.STONE)));
 
-    public static final RegistryObject<Block> BEAM_EMITTER = BLOCKS.register("beam_emitter", BeamEmitter::new);
+    public static final RegistryObject<Block> BEAM_EMITTER = registerBlock("beam_emitter", BeamEmitter::new);
 
 }
--- src/main/java/com/harby/halocraft/core/HaloItems.java
+++ src/main/java/com/harby/halocraft/core/HaloItems.java
@@ -45,22 +45,4 @@ public class HaloItems {
     private static RegistryObject<Item> block(RegistryObject<Block> block) {
         return ITEMS.register(block.getId().getPath(), () -> new BlockItemBase(block.get(), new Item.Properties()));
     }
-    public static final RegistryObject<Item> CONCRETE_BRICK = block(HaloBlocks.CONCRETE_BRICK);
-    public static final RegistryObject<Item> CONCRETE_BRICK_POLISHED = block(HaloBlocks.CONCRETE_BRICK_POLISHED);
-    public static final RegistryObject<Item> CONCRETE_LAYERED = block(HaloBlocks.CONCRETE_LAYERED);
-    public static final RegistryObject<Item> CONCRETE_LAYERED_SECOND = block(HaloBlocks.CONCRETE_LAYERED_SECOND);
-
-    public static final RegistryObject<Item> ASPHALT = block(HaloBlocks.ASPHALT);
-    public static final RegistryObject<Item> CONCRETE_YELLOW_DOUBLE = block(HaloBlocks.CONCRETE_YELLOW_DOUBLE);
-    public static final RegistryObject<Item> WHITE_LINE = block(HaloBlocks.WHITE_LINE);
-    public static final RegistryObject<Item> WHITE_LINE_DOTS = block(HaloBlocks.WHITE_LINE_DOTS);
-    public static final RegistryObject<Item> YELLOW_DOUBLE = block(HaloBlocks.YELLOW_DOUBLE);
-    public static final RegistryObject<Item> YELLOW_DOUBLE_DOT = block(HaloBlocks.YELLOW_DOUBLE_DOT);
-    public static final RegistryObject<Item> YELLOW_LINE = block(HaloBlocks.YELLOW_LINE);
-
-    public static final RegistryObject<Item> ASTEROID = block(HaloBlocks.ASTEROID);
-    public static final RegistryObject<Item> DENSE_ASTEROID = block(HaloBlocks.DENSE_ASTEROID);
-
-    public static final RegistryObject<Item> BEAM_EMITTER = block(HaloBlocks.BEAM_EMITTER);
-
 }
