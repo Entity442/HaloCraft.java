84028e3069c9ebc410a4cb1701c7d0a8c89c3fe3
Youritch <86657843+Youritch@users.noreply.github.com>
2024-01-02 09:46:01 +0100

    fill the tags

--- src/main/java/com/harby/halocraft/datagen/ModBlockTagGenerator.java
+++ src/main/java/com/harby/halocraft/datagen/ModBlockTagGenerator.java
@@ -1,8 +1,13 @@
 package com.harby.halocraft.datagen;
 
 import com.harby.halocraft.HaloCraft;
+import com.harby.halocraft.core.HaloTags;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
@@ -19,6 +24,13 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.@NotNull Provider pProvider) {
         //Custom tags ->
+        this.tag(HaloTags.Blocks.BREAK_ON_SHOOT)
+                .addTag(Tags.Blocks.GLASS)
+                .addTag(Tags.Blocks.GLASS_PANES);
+
+        this.tag(HaloTags.Blocks.SHOOTING_THROUGH)
+                .addTag(Tags.Blocks.FENCES)
+                .add(Blocks.IRON_BARS);
 
     }
 }
