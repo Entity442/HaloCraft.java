48cccf2858341b565543d144c36c0f301555c21d
Youritch <86657843+Youritch@users.noreply.github.com>
2024-01-18 21:37:30 +0100

    work on new systme (this class is still a test for scope gun)

--- src/main/java/com/harby/halocraft/HaloItems/SniperRiffle.java
+++ src/main/java/com/harby/halocraft/HaloItems/SniperRiffle.java
@@ -1,19 +1,16 @@
 package com.harby.halocraft.HaloItems;
 
-import com.harby.halocraft.HaloEntities.Projectiles.BaseBulletEntity;
-import net.minecraft.world.entity.LivingEntity;
+import com.harby.halocraft.core.projectiles.AmmoTypes;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
-import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
-public class SniperRiffle extends Gun{
-    public static final int USE_DURATION = 1200;
+public class SniperRiffle extends Gun {
+    //this class work as a test for scope gun
     public static final float ZOOM_FOV_MODIFIER = 0.1F;
-    private final boolean golden;
-    public SniperRiffle(Properties properties,boolean gold) {
-        super(properties, ammoType);
-        this.golden = gold;
+
+    public SniperRiffle(Properties properties, boolean golden) {
+        super(properties, true, AmmoTypes.BULLET, 4, 20, 200, golden ? 25.0f : 35.0f, 1000);
     }
 
     public @NotNull UseAnim getUseAnimation(ItemStack pStack) {
@@ -23,17 +20,6 @@ public class SniperRiffle extends Gun{
     @Override
     public int getShootingDelay() {
         return 20;
-    }
-
-    @Override
-    public void shotProjectile(Level level, LivingEntity livingEntity, ItemStack stack) {
-        if (!level.isClientSide) {
-            BaseBulletEntity bulletEntity = new BaseBulletEntity(level,livingEntity);
-            bulletEntity.setProjectileType(getAmmoType(stack));
-            bulletEntity.setDamage(golden ? 35f : 20f);
-            bulletEntity.shootFromRotation(livingEntity, livingEntity.getXRot(), livingEntity.getYRot(), 0.0F, 6.0F, 1.0F);
-            level.addFreshEntity(bulletEntity);
-        }
     }
 
     @Override
@@ -51,7 +37,7 @@ public class SniperRiffle extends Gun{
         return true;
     }
 
-    public boolean isScopeing(){
+    public boolean isScopeing() {
         return true;
     }
 }
