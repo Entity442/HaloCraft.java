282e9b98524fd67d0759009520c9adaa31d828b5
Youritch <86657843+Youritch@users.noreply.github.com>
2024-01-18 21:34:57 +0100

    rename things

--- src/main/java/com/harby/halocraft/Client/Renderers/BulletRenderer.java
+++ src/main/java/com/harby/halocraft/Client/Renderers/BulletRenderer.java
@@ -1,14 +1,14 @@
 package com.harby.halocraft.Client.Renderers;
 
 import com.harby.halocraft.HaloCraft;
-import com.harby.halocraft.HaloEntities.Projectiles.BaseBulletEntity;
+import com.harby.halocraft.HaloEntities.Projectiles.BulletEntity;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
-public class BulletRenderer <T extends BaseBulletEntity> extends EntityRenderer<T> {
-    public BulletRenderer(EntityRendererProvider.Context p_174008_) {
-        super(p_174008_);
+public class BulletRenderer <T extends BulletEntity> extends EntityRenderer<T> {
+    public BulletRenderer(EntityRendererProvider.Context pContext) {
+        super(pContext);
     }
 
     @Override
