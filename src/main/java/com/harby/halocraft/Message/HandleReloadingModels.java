20ecc13d5f465838b4aea3fd21127550fb4388a1
Youritch <86657843+Youritch@users.noreply.github.com>
2024-02-01 19:37:35 +0100

    rename parameters

--- src/main/java/com/harby/halocraft/Message/HandleReloadingModels.java
+++ src/main/java/com/harby/halocraft/Message/HandleReloadingModels.java
@@ -5,6 +5,7 @@ import com.harby.halocraft.HaloItems.Gun;
 import com.harby.halocraft.core.HaloItems;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 
 public class HandleReloadingModels {
     public static void addReloading() {
@@ -15,17 +16,17 @@ public class HandleReloadingModels {
     }
 
     private static void makeGun(Gun item) {
-        ItemProperties.register(item, new ResourceLocation("shooting"), (pStack, p_174606_, property, p_174608_) -> {
-            return property != null && item.isShooting() ? 1.0F : 0.0F;
+        ItemProperties.register(item, new ResourceLocation("shooting"), (pStack, level, livingEntity, p_174608_) -> {
+            return livingEntity != null && item.isShooting() ? 1.0F : 0.0F;
         });
-        ItemProperties.register(item, new ResourceLocation("reloading"), (pStack, p_174606_, property, p_174608_) -> {
-            return property != null && item.isReloading() ? 1.0F : 0.0F;
+        ItemProperties.register(item, new ResourceLocation("reloading"), (pStack, level, livingEntity, p_174608_) -> {
+            return livingEntity != null && item.isReloading((Player) (livingEntity)) ? 1.0F : 0.0F;
         });
     }
 
     private static void makeEnergySword(EnergieSword item) {
-        ItemProperties.register(item, new ResourceLocation("is_off"), (pStack, p_174606_, property, p_174608_) -> {
-            return property != null && item.isOff(pStack) ? 1.0F : 0.0F;
+        ItemProperties.register(item, new ResourceLocation("is_off"), (pStack, level, livingEntity, p_174608_) -> {
+            return livingEntity != null && item.isOff(pStack) ? 1.0F : 0.0F;
         });
     }
 }
