bc9c8d87c4a90691d64ccc4f10ab49c8cd13edd2
Youritch <86657843+Youritch@users.noreply.github.com>
2024-02-11 15:50:12 +0100

    add RotatedPillarBlock model/blockstate generator

--- src/main/java/com/harby/halocraft/datagen/ModBlockStateProvider.java
+++ src/main/java/com/harby/halocraft/datagen/ModBlockStateProvider.java
@@ -3,13 +3,14 @@ package com.harby.halocraft.datagen;
 import com.harby.halocraft.HaloCraft;
 import com.harby.halocraft.core.HaloBlocks;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockStateProvider extends BlockStateProvider {
-
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
         super(output, HaloCraft.MODID, exFileHelper);
     }
@@ -20,12 +21,32 @@ public class ModBlockStateProvider extends BlockStateProvider {
             if (ConfigDataGenerator.excludesBlocksGenerate.contains(blockRegistryObject.get())) {
                 continue;
             }
-            blockWithItem(blockRegistryObject);
+            if (blockRegistryObject.get() instanceof RotatedPillarBlock rpBlock) {
+                rotatedPillarBlockWithItem(rpBlock);
+            } else {
+                blockWithItem(blockRegistryObject.get());
+            }
         }
 
     }
 
-    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
-        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    private void blockWithItem(Block blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject, cubeAll(blockRegistryObject));
+    }
+
+    private void rotatedPillarBlockWithItem(RotatedPillarBlock blockRegistryObject) {
+        ResourceLocation texture = blockTexture(blockRegistryObject);
+        HaloCraft.LOGGER.info(name(blockRegistryObject));
+        axisBlock(
+                blockRegistryObject,
+                models().cubeColumnHorizontal(name(blockRegistryObject), texture, texture),
+                models().cubeColumn(name(blockRegistryObject), texture, texture)
+        );
+        itemModels().getBuilder(name(blockRegistryObject)).parent(cubeAll(blockRegistryObject));
+    }
+
+    private String name(Block block) {
+        return block.getDescriptionId().split("\\.")[2];
     }
 }
+
