8b398463a6e4e3d7bd188c4e8b0fa17d2331e953
Youritch <86657843+Youritch@users.noreply.github.com>
2024-02-17 16:37:28 +0100

    can't render if not firstPerson

--- src/main/java/com/harby/halocraft/Events/ForgeEvents.java
+++ src/main/java/com/harby/halocraft/Events/ForgeEvents.java
@@ -2,6 +2,7 @@ package com.harby.halocraft.Events;
 
 import com.harby.halocraft.HaloCraft;
 import com.harby.halocraft.HaloItems.ScopeGun;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.event.ComputeFovModifierEvent;
@@ -12,6 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 public class ForgeEvents {
     @SubscribeEvent
     public static void ComputeFovModifierEvent(ComputeFovModifierEvent event) {
+        if (!Minecraft.getInstance().options.getCameraType().isFirstPerson()) return;
         Player player = event.getPlayer();
         ItemStack stack = player.getItemInHand(player.getUsedItemHand());
         if (stack.getItem() instanceof ScopeGun gun && gun.isScoping(stack)) {
