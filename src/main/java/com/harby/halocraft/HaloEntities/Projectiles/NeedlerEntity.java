2a9082a99b9e3910ba3aff7268831e6231e7fa27
Youritch <86657843+Youritch@users.noreply.github.com>
2024-04-13 14:47:49 +0200

    security

--- src/main/java/com/harby/halocraft/HaloEntities/Projectiles/NeedlerEntity.java
+++ src/main/java/com/harby/halocraft/HaloEntities/Projectiles/NeedlerEntity.java
@@ -1,6 +1,7 @@
 package com.harby.halocraft.HaloEntities.Projectiles;
 
 import com.harby.halocraft.core.projectiles.AmmoList;
+import com.harby.halocraft.core.projectiles.AmmoTypes;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
@@ -8,6 +9,9 @@ import net.minecraft.world.level.Level;
 public class NeedlerEntity extends BaseProjectileEntity{
     public NeedlerEntity(Level level, Entity livingEntity, AmmoList ammo, EntityType<? extends BaseProjectileEntity> entityType, double speed) {
         super(level, livingEntity, ammo, entityType, speed);
+        if (ammo.getType() != AmmoTypes.NEEDLER) {
+            throw new IllegalArgumentException("NeedlerEntity can only be created with Needler ammo.");
+        }
     }
 
     public NeedlerEntity(Level level, EntityType<? extends BaseProjectileEntity> entityType) {
